0f25c4d6b60e7dd968706571b0d198a6a9d6d09b
EricWerk <eric.werk@gmail.com>
2020-03-16 02:13:17 +0100

    Initial commit

---
+++ Entities/Artikel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artikler.Entities
+{
+    public class Artikel
+    {
+        public int Id { get; set; }
+        public string Forfatter { get; set; }
+        public string Overskrift { get; set; }
+        public string Tekst { get; set; }
+        public int Årstal { get; set; }
+    }
+}
