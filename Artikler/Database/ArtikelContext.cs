92fa0d4ef5f942f14549bab9cef74ae5439f508b
EricWerk <eric.werk@gmail.com>
2020-03-16 17:18:35 +0100

    Incorrect folder structure fixed

---
+++ Artikler/Database/ArtikelContext.cs
@@ -0,0 +1,19 @@
+﻿using Artikler.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Artikler.Database
+{
+    public class ArtikelContext : DbContext
+    {
+        public ArtikelContext(DbContextOptions<ArtikelContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Artikel> Artikler { get; set; }
+    }
+}
---
+++ Artikler/Migrations/20200315233014_CreateDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Artikler.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artikler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Forfatter = table.Column<string>(nullable: true),
+                    Overskrift = table.Column<string>(nullable: true),
+                    Tekst = table.Column<string>(nullable: true),
+                    Årstal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artikler", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artikler");
+        }
+    }
+}
