509812fa05c2166ddc3a4a0b6aa60c6b88b1fe94
Eric Werk <eric.werk@gmail.com>
2020-03-19 22:04:33 +0100

    Documentation, Update

--- Artikler/Controllers/ArtikelController.cs
+++ Artikler/Controllers/ArtikelController.cs
@@ -28,14 +28,24 @@ namespace Artikler.Controllers
             _context = context;
         }
 
+        [Route("/Artikel")]
+        public IActionResult Documentation()
+        {
+            var host = ControllerContext.HttpContext.Request.Host.ToString();
+            var actions = new Dictionary<string, string>();
+            actions.Add("List",  $"{host}/Artikler/List");
+
+            return new JsonResult(actions);
+        }
+
         /// <summary>
         /// Liste af artikler
         /// </summary>
-        /// <param name="skip"></param>
-        /// <param name="take"></param>
+        /// <param name="skip">Springer over det angivne antal artikler</param>
+        /// <param name="take">begrænser antallet af artikler i resultatsættet</param>
         /// <returns></returns>
         [HttpGet]
-        public Task<List<ArtikelHoved>> List(int skip = 0, int take = 100)
+        public Task<List<ArtikelHoved>> List(int skip = 0, int take = 10)
         {
             if (skip <= 0) skip = 0;
             if (take <= 0) take = 1;
@@ -44,6 +54,8 @@ namespace Artikler.Controllers
                 .Artikler
                 .OrderByDescending(a => a.Årstal)
                 .ThenBy(a => a.Forfatter)
+                .ThenBy(a => a.Overskrift)
+                .ThenBy(a => a.Id)
                 .Skip(skip)
                 .Take(take)
                 .Select( a => new ArtikelHoved(a))
@@ -71,7 +83,7 @@ namespace Artikler.Controllers
         [HttpPost]
         public void Create(int year, string author, string title, string content)
         {
-            var x = _context.Artikler.Add(new Artikel
+            _context.Artikler.Add(new Artikel
             {
                 Årstal = year,
                 Forfatter = author,
@@ -79,22 +91,55 @@ namespace Artikler.Controllers
                 Tekst = content
             });
             _context.SaveChanges();
+        }
 
+        /// <summary>
+        /// Tilføj en ny artikel
+        /// </summary>
+        /// <param name="year">Arstallet artiklen er skevet</param>
+        /// <param name="author">Forfatter</param>
+        /// <param name="title">En god overskrift</param>
+        /// <param name="content">En fed historie</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async void Update(Artikel artikel)
+        {
+            if (null != artikel?.Id)
+            {
+                var a = await _context.Artikler.FindAsync(artikel.Id);
+                a.Årstal = artikel.Årstal;
+                a.Forfatter = artikel.Forfatter;
+                a.Overskrift = artikel.Overskrift;
+                a.Tekst = artikel.Tekst;
+
+                _context.SaveChanges();
+            }
         }
 
         /// <summary>
         /// Søg efter artikler
         /// </summary>
+        /// <param name="skip">Springer over det angivne antal artikler</param>
+        /// <param name="take">begrænser antallet af artikler i resultatsættet</param>
         /// <param name="query">Søge kriterie</param>
         /// <returns></returns>
         [HttpGet]
-        public Task<List<ArtikelHoved>> Search(string query)
+        public Task<List<ArtikelHoved>> Search(string query, int skip = 0, int take = 100)
         {
+            if (string.IsNullOrEmpty(query))
+            {
+                RedirectToAction("List");
+            }
+
             return _context
                 .Artikler
                 .OrderByDescending(a => a.Årstal)
                 .ThenBy(a => a.Forfatter)
+                .ThenBy(a => a.Overskrift)
+                .ThenBy(a => a.Id)
                 .Where(a => a.Forfatter.Contains(query) || a.Overskrift.Contains(query) || a.Tekst.Contains(query))
+                .Skip(skip)
+                .Take(take)
                 .Select(a => new ArtikelHoved(a))
                 .ToListAsync();
         }
